43d08d647f37e325bb3e7584b39003fdea0f140b
thscharler <thscharler@gmail.com>
2024-03-10 22:30:13 +0100

    refactor: rename FrameExt->RenderFrameWidget and WidgetExt to FrameWidget.
    refactor: rename try_ui! to err!
    refactor: add yeet! for error short circuit on ControlUI.

--- src/lib.rs
+++ src/lib.rs
@@ -20,22 +20,23 @@ pub(crate) mod util;
 
 pub use framework::{run_tui, TaskSender, ThreadPool, TuiApp};
 
-/// Extra rendering with passes on the frame to a WidgetExt.
-/// Allows setting the cursor in a component.
-pub trait FrameExt {
-    fn render_ext<W: WidgetExt>(&mut self, widget: W, area: Rect, state: &mut W::State);
+/// Extra rendering which passes on the frame to a [FrameWidget].
+/// This allows setting the cursor inside a widget.
+pub trait RenderFrameWidget {
+    fn render_frame_widget<W: FrameWidget>(&mut self, widget: W, area: Rect, state: &mut W::State);
 }
 
-impl<'a> FrameExt for Frame<'a> {
-    fn render_ext<W: WidgetExt>(&mut self, widget: W, area: Rect, state: &mut W::State) {
+impl<'a> RenderFrameWidget for Frame<'a> {
+    fn render_frame_widget<W: FrameWidget>(&mut self, widget: W, area: Rect, state: &mut W::State) {
         widget.render(self, area, state)
     }
 }
 
 /// Another kind of widget that takes a frame instead of a buffer.
 /// Allows to set the cursor while rendering.
+///
 /// This also always takes a state, just use () if not needed.
-pub trait WidgetExt {
+pub trait FrameWidget {
     /// Type of the corresponding state struct.
     type State: ?Sized;
 
@@ -50,10 +51,10 @@ pub trait HandleEvent<Action, Err> {
     fn handle(&mut self, evt: &Event) -> ControlUI<Action, Err>;
 }
 
-/// Try macro that uses ControlUI instead of Result for control-flow.
-/// Converts a Result::Err into a ControlUI::Err with into() conversion.
+/// Converts from a [Result::Err] to a [ControlUI::Err] and returns early.
+/// Evaluates to the value of [Result::Ok].
 #[macro_export]
-macro_rules! try_ui {
+macro_rules! err {
     ($ex:expr) => {{
         match $ex {
             Ok(v) => v,
@@ -62,15 +63,28 @@ macro_rules! try_ui {
     }};
 }
 
-/// Cuts the control-flow. If the value is not ControlUI::Continue it returns early.
+/// Cuts the control-flow. If the value is not [ControlUI::Continue] it returns early.
 #[macro_export]
 macro_rules! cut {
-    ($x:expr) => {
+    ($x:expr) => {{
         let r = $x;
         if !r.is_continue() {
             return r;
         }
-    };
+    }};
+}
+
+/// Cuts the control-flow. If the value is [ControlUI::Err] it returns early.
+/// Evaluates to any other value.
+#[macro_export]
+macro_rules! yeet {
+    ($x:expr) => {{
+        let r = $x;
+        if r.is_err() {
+            return r;
+        }
+        r
+    }};
 }
 
 /// UI control flow.
@@ -78,7 +92,7 @@ macro_rules! cut {
 /// This is the result type for an event-handler.
 ///
 /// * Continue - Continue with execution.
-/// * Err(Err) - Equivalent to Result::Err. Use the macro [try_ui] to convert from Result.
+/// * Err(Err) - Equivalent to Result::Err. Use the macro [err] to convert from Result.
 /// * Unchanged - Event processed, no UI update necessary.
 /// * Changed - Event processed, UI update necessary.
 /// * Action(Action) - Run some computation on the model.
@@ -86,7 +100,9 @@ macro_rules! cut {
 /// * Break - Break the event loop; end the program.
 ///
 /// There are multiple continuation functions that work with these states.
+/// And the macros [err!], [cut!] and [yeet!]
 #[derive(Debug)]
+#[must_use]
 pub enum ControlUI<Action, Err> {
     /// Continue execution.
     Continue,
@@ -105,30 +121,37 @@ pub enum ControlUI<Action, Err> {
 }
 
 impl<Action, Err> ControlUI<Action, Err> {
+    /// Continue case
     pub fn is_continue(&self) -> bool {
         matches!(self, ControlUI::Continue)
     }
 
+    /// Err case
     pub fn is_err(&self) -> bool {
         matches!(self, ControlUI::Err(_))
     }
 
+    /// Unchanged case
     pub fn is_unchanged(&self) -> bool {
         matches!(self, ControlUI::Unchanged)
     }
 
+    /// Changed case
     pub fn is_changed(&self) -> bool {
         matches!(self, ControlUI::Changed)
     }
 
+    /// Action
     pub fn is_action(&self) -> bool {
         matches!(self, ControlUI::Action(_))
     }
 
+    /// Background action
     pub fn is_spawn(&self) -> bool {
         matches!(self, ControlUI::Spawn(_))
     }
 
+    /// Break case.
     pub fn is_break(&self) -> bool {
         matches!(self, ControlUI::Break)
     }
@@ -175,7 +198,7 @@ impl<Action, Err> ControlUI<Action, Err> {
         }
     }
 
-    /// Just convert the error to another type with into().
+    /// Convert an error to another error type with into().
     pub fn err_into<F>(self) -> ControlUI<Action, F>
     where
         Err: Into<F>,
@@ -194,7 +217,7 @@ impl<Action, Err> ControlUI<Action, Err> {
     /// Run the continuation if the value is Action or Spawn.
     ///
     /// Allows the result action to differ from the input to convert
-    /// component actions to more global ones.
+    /// widget actions to more global ones.
     ///
     /// Caveat: Allows no differentiation between Action and Spawn.
     pub fn and_then<B>(self, f: impl FnOnce(Action) -> ControlUI<B, Err>) -> ControlUI<B, Err> {
@@ -225,7 +248,7 @@ impl<Action, Err> ControlUI<Action, Err> {
         }
     }
 
-    /// Run the continuation if the value is Changed.
+    /// Run the if the value is Unchanged. Allows to return some value.
     pub fn on_unchanged_do<R>(&self, f: impl FnOnce() -> R) -> Option<R> {
         match self {
             ControlUI::Unchanged => Some(f()),
@@ -233,7 +256,7 @@ impl<Action, Err> ControlUI<Action, Err> {
         }
     }
 
-    /// Run the continuation if the value is Changed
+    /// Run the continuation if the value is Changed.
     pub fn on_changed(self, f: impl FnOnce() -> ControlUI<Action, Err>) -> ControlUI<Action, Err> {
         match self {
             ControlUI::Continue => ControlUI::Continue,
@@ -246,7 +269,7 @@ impl<Action, Err> ControlUI<Action, Err> {
         }
     }
 
-    /// Run the continuation if the value is Changed
+    /// Run if the value is Changed. Allows to return some value.
     pub fn on_changed_do<R>(&self, f: impl FnOnce() -> R) -> Option<R> {
         match self {
             ControlUI::Changed => Some(f()),
