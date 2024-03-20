bd9fcc9bc9c470240e4a0d3b7d10fd7cd6a3b7f9
thscharler <thscharler@gmail.com>
2024-03-20 21:09:54 +0100

    example: form example

---
+++ examples/edit_form.rs
@@ -0,0 +1,537 @@
+#![allow(unused_variables)]
+
+use crossterm::event::Event;
+use rat_salsa::layout::{layout_edit, EditConstraint};
+use rat_salsa::widget::button::ButtonStyle;
+use rat_salsa::widget::input::{TextInput, TextInputState, TextInputStyle};
+use rat_salsa::widget::mask_input::{MaskedInput, MaskedInputState, MaskedInputStyle};
+use rat_salsa::widget::menuline::{MenuLine, MenuLineState, MenuStyle};
+use rat_salsa::widget::message::{
+    StatusDialog, StatusDialogState, StatusDialogStyle, StatusLine, StatusLineState,
+};
+use rat_salsa::{
+    check_break, run_tui, try_ui, ControlUI, DefaultKeys, Focus, HandleCrossterm, HasFocusFlag,
+    RenderFrameWidget, Repaint, RepaintEvent, RunConfig, TaskSender, ThreadPool, TimerEvent,
+    Timers, TuiApp,
+};
+use ratatui::layout::{Constraint, Direction, Layout, Rect};
+use ratatui::prelude::{Color, Style};
+use ratatui::style::Stylize;
+use ratatui::text::Span;
+use ratatui::Frame;
+use std::time::SystemTime;
+
+fn main() -> Result<(), anyhow::Error> {
+    setup_logging()?;
+
+    let mut data = FormOneData::default();
+    let mut state = FormOneState::default();
+
+    run_tui(
+        &FormOneApp,
+        &mut data,
+        &mut state,
+        RunConfig {
+            n_threats: 1,
+            log_timing: false,
+            ..RunConfig::default()
+        },
+    )?;
+
+    Ok(())
+}
+
+// -----------------------------------------------------------------------
+
+type Control = ControlUI<FormOneAction, anyhow::Error>;
+
+#[derive(Debug, Default)]
+pub struct FormOneData {}
+
+#[derive(Debug)]
+pub enum FormOneAction {}
+
+#[derive(Debug, Default)]
+pub struct FormOneState {
+    pub g: GeneralState,
+    pub mask0: Mask0,
+}
+
+#[derive(Debug)]
+pub struct GeneralState {
+    pub theme: &'static Theme,
+    pub repaint: Repaint,
+    pub timers: Timers,
+    pub status: StatusLineState,
+    pub error_dlg: StatusDialogState,
+}
+
+#[derive(Debug)]
+pub struct Mask0 {
+    pub menu: MenuLineState<u16>,
+
+    pub text: TextInputState,
+    pub decimal: TextInputState,
+    pub float: TextInputState,
+    pub ipv4: MaskedInputState,
+    pub hexcolor: MaskedInputState,
+    pub creditcard: MaskedInputState,
+    pub date: MaskedInputState,
+}
+
+impl Default for GeneralState {
+    fn default() -> Self {
+        Self {
+            theme: &ONEDARK,
+            repaint: Default::default(),
+            timers: Default::default(),
+            status: Default::default(),
+            error_dlg: Default::default(),
+        }
+    }
+}
+
+impl Default for Mask0 {
+    fn default() -> Self {
+        let mut s = Self {
+            menu: Default::default(),
+            text: Default::default(),
+            decimal: Default::default(),
+            float: Default::default(),
+            ipv4: Default::default(),
+            hexcolor: Default::default(),
+            creditcard: Default::default(),
+            date: Default::default(),
+        };
+        s.menu.focus.set();
+        s.text.focus.set();
+        s.ipv4.set_mask("999.999.999.999");
+        s.ipv4.set_display_mask("xxx.xxx.xxx.xxx");
+        s.hexcolor.set_mask("HHHHHH");
+        s.creditcard.set_mask("9999 9999 9999 9999");
+        s.creditcard.set_display_mask("____ ____ ____ ____");
+        s.date.set_mask("99/99/9999");
+        s.date.set_display_mask("mm/dd/yyyy");
+        s
+    }
+}
+
+// -----------------------------------------------------------------------
+
+#[derive(Debug)]
+pub struct FormOneApp;
+
+#[derive(Debug, Clone, Copy)]
+pub struct FormOneAppLayout {
+    area: Rect,
+    menu: Rect,
+    status: Rect,
+}
+
+impl TuiApp for FormOneApp {
+    type Data = FormOneData;
+    type State = FormOneState;
+    type Action = FormOneAction;
+    type Error = anyhow::Error;
+
+    fn get_repaint<'b>(&self, uistate: &'b Self::State) -> Option<&'b Repaint> {
+        Some(&uistate.g.repaint)
+    }
+
+    fn get_timers<'b>(&self, uistate: &'b Self::State) -> Option<&'b Timers> {
+        Some(&uistate.g.timers)
+    }
+
+    fn repaint(
+        &self,
+        event: RepaintEvent,
+        frame: &mut Frame<'_>,
+        data: &mut Self::Data,
+        uistate: &mut Self::State,
+    ) -> ControlUI<Self::Action, Self::Error> {
+        let area = frame.size();
+
+        let layout = {
+            let r = Layout::new(
+                Direction::Vertical,
+                [
+                    Constraint::Fill(1),
+                    Constraint::Length(1),
+                    Constraint::Length(1),
+                ],
+            )
+            .split(area);
+
+            FormOneAppLayout {
+                area: r[0],
+                menu: r[1],
+                status: r[2],
+            }
+        };
+
+        try_ui!(repaint_mask0(&event, frame, layout, data, uistate), _);
+
+        if uistate.g.error_dlg.active {
+            let err = StatusDialog::new().style(uistate.g.theme.status_dialog_style());
+            frame.render_stateful_widget(err, layout.area, &mut uistate.g.error_dlg);
+        }
+
+        let status = StatusLine::new().style(uistate.g.theme.status_style());
+        frame.render_stateful_widget(status, layout.status, &mut uistate.g.status);
+
+        Control::Continue
+    }
+
+    fn handle_timer(
+        &self,
+        event: TimerEvent,
+        data: &mut Self::Data,
+        uistate: &mut Self::State,
+    ) -> ControlUI<Self::Action, Self::Error> {
+        // TODO: timer
+        Control::Continue
+    }
+
+    fn handle_event(
+        &self,
+        event: Event,
+        data: &mut Self::Data,
+        uistate: &mut Self::State,
+    ) -> ControlUI<Self::Action, Self::Error> {
+        use crossterm::event::*;
+
+        check_break!(match &event {
+            Event::Resize(_, _) => {
+                //
+                Control::Change
+            }
+            Event::Key(KeyEvent {
+                kind: KeyEventKind::Press,
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::CONTROL,
+                ..
+            }) => {
+                //
+                Control::Break
+            }
+            _ => Control::Continue,
+        });
+
+        check_break!({
+            if uistate.g.error_dlg.active {
+                uistate.g.error_dlg.handle(&event, DefaultKeys)
+            } else {
+                Control::Continue
+            }
+        });
+
+        check_break!(handle_mask0(&event, data, uistate));
+
+        Control::Continue
+    }
+
+    fn run_action(
+        &self,
+        action: Self::Action,
+        data: &mut Self::Data,
+        uistate: &mut Self::State,
+        worker: &ThreadPool<Self>,
+    ) -> ControlUI<Self::Action, Self::Error> {
+        // TODO: actions
+        Control::Continue
+    }
+
+    fn run_task(
+        &self,
+        task: Self::Action,
+        send: &TaskSender<Self>,
+    ) -> ControlUI<Self::Action, Self::Error> {
+        // TODO: tasks
+        Control::Continue
+    }
+
+    fn report_error(
+        &self,
+        error: Self::Error,
+        data: &mut Self::Data,
+        uistate: &mut Self::State,
+    ) -> ControlUI<Self::Action, Self::Error> {
+        uistate.g.error_dlg.log(format!("{:?}", &*error).as_str());
+        Control::Change
+    }
+}
+
+fn repaint_mask0(
+    event: &RepaintEvent,
+    frame: &mut Frame<'_>,
+    layout: FormOneAppLayout,
+    data: &mut FormOneData,
+    uistate: &mut FormOneState,
+) -> Control {
+    // TODO: repaint_mask
+
+    let l = layout_edit(
+        layout.area,
+        [
+            EditConstraint::Label("Text"),
+            EditConstraint::Widget(20),
+            EditConstraint::Label("Integer"),
+            EditConstraint::Widget(12),
+            EditConstraint::Label("Float"),
+            EditConstraint::Widget(12),
+            EditConstraint::Label("IPv4"),
+            EditConstraint::Widget(16),
+            EditConstraint::Label("Color"),
+            EditConstraint::Widget(7),
+            EditConstraint::Label("Credit card"),
+            EditConstraint::Widget(20),
+            EditConstraint::Label("Date"),
+            EditConstraint::Widget(11),
+        ],
+    );
+
+    let w_text = TextInput::default().style(uistate.g.theme.input_style());
+    let w_decimal = TextInput::default().style(uistate.g.theme.input_style());
+    let w_float = TextInput::default().style(uistate.g.theme.input_style());
+    let w_color = MaskedInput::default().style(uistate.g.theme.input_mask_style());
+    let w_ipv4 = MaskedInput::default().style(uistate.g.theme.input_mask_style());
+    let w_creditcard = MaskedInput::default().style(uistate.g.theme.input_mask_style());
+    let w_date = MaskedInput::default().style(uistate.g.theme.input_mask_style());
+
+    frame.render_widget(Span::from("Text"), l.label[0]);
+    frame.render_frame_widget(w_text, l.widget[0], &mut uistate.mask0.text);
+    frame.render_widget(Span::from("Integer"), l.label[1]);
+    frame.render_frame_widget(w_decimal, l.widget[1], &mut uistate.mask0.decimal);
+    frame.render_widget(Span::from("Float"), l.label[2]);
+    frame.render_frame_widget(w_float, l.widget[2], &mut uistate.mask0.float);
+    frame.render_widget(Span::from("IPv4"), l.label[3]);
+    frame.render_frame_widget(w_ipv4, l.widget[3], &mut uistate.mask0.ipv4);
+    frame.render_widget(Span::from("Color"), l.label[4]);
+    frame.render_frame_widget(w_color, l.widget[4], &mut uistate.mask0.hexcolor);
+    frame.render_widget(Span::from("Credit card"), l.label[5]);
+    frame.render_frame_widget(w_creditcard, l.widget[5], &mut uistate.mask0.creditcard);
+    frame.render_widget(Span::from("Date"), l.label[6]);
+    frame.render_frame_widget(w_date, l.widget[6], &mut uistate.mask0.date);
+
+    let menu = MenuLine::new()
+        .style(uistate.g.theme.menu_style())
+        .add("_Quit", 0u16);
+    frame.render_stateful_widget(menu, layout.menu, &mut uistate.mask0.menu);
+
+    Control::Continue
+}
+
+fn focus0(mask0: &Mask0) -> Focus<'_> {
+    Focus::new([
+        (mask0.text.focus(), mask0.text.area()),
+        (mask0.decimal.focus(), mask0.decimal.area()),
+        (mask0.float.focus(), mask0.float.area()),
+        (mask0.ipv4.focus(), mask0.ipv4.area()),
+        (mask0.hexcolor.focus(), mask0.hexcolor.area()),
+        (mask0.creditcard.focus(), mask0.creditcard.area()),
+        (mask0.date.focus(), mask0.date.area()),
+    ])
+}
+
+fn handle_mask0(event: &Event, data: &mut FormOneData, uistate: &mut FormOneState) -> Control {
+    let mask0 = &mut uistate.mask0;
+
+    focus0(mask0)
+        .handle(event, DefaultKeys)
+        .and_do(|_| uistate.g.repaint.set());
+
+    // TODO: handle_mask
+    check_break!(mask0.text.handle(event, DefaultKeys));
+    check_break!(mask0.decimal.handle(event, DefaultKeys));
+    check_break!(mask0.float.handle(event, DefaultKeys));
+    check_break!(mask0.ipv4.handle(event, DefaultKeys));
+    check_break!(mask0.hexcolor.handle(event, DefaultKeys));
+    check_break!(mask0.creditcard.handle(event, DefaultKeys));
+    check_break!(mask0.date.handle(event, DefaultKeys));
+
+    check_break!(mask0.menu.handle(event, DefaultKeys).and_then(|a| match a {
+        0 => {
+            Control::Break
+        }
+        _ => Control::Continue,
+    }));
+
+    Control::Continue
+}
+
+// -----------------------------------------------------------------------
+
+#[derive(Debug, Default)]
+pub struct Theme {
+    pub name: &'static str,
+    pub dark_theme: bool,
+
+    pub white: Color,
+    pub darker_black: Color,
+    pub black: Color,
+    pub black2: Color,
+    pub one_bg: Color,
+    pub one_bg2: Color,
+    pub one_bg3: Color,
+    pub grey: Color,
+    pub grey_fg: Color,
+    pub grey_fg2: Color,
+    pub light_grey: Color,
+    pub red: Color,
+    pub baby_pink: Color,
+    pub pink: Color,
+    pub line: Color,
+    pub green: Color,
+    pub vibrant_green: Color,
+    pub nord_blue: Color,
+    pub blue: Color,
+    pub yellow: Color,
+    pub sun: Color,
+    pub purple: Color,
+    pub dark_purple: Color,
+    pub teal: Color,
+    pub orange: Color,
+    pub cyan: Color,
+    pub statusline_bg: Color,
+    pub lightbg: Color,
+    pub pmenu_bg: Color,
+    pub folder_bg: Color,
+
+    pub base00: Color,
+    pub base01: Color,
+    pub base02: Color,
+    pub base03: Color,
+    pub base04: Color,
+    pub base05: Color,
+    pub base06: Color,
+    pub base07: Color,
+    pub base08: Color,
+    pub base09: Color,
+    pub base0a: Color,
+    pub base0b: Color,
+    pub base0c: Color,
+    pub base0d: Color,
+    pub base0e: Color,
+    pub base0f: Color,
+}
+
+impl Theme {
+    pub fn status_style(&self) -> Style {
+        Style::default().fg(self.white).bg(self.one_bg3)
+    }
+
+    pub fn input_style(&self) -> TextInputStyle {
+        TextInputStyle {
+            style: Style::default().fg(self.black).bg(self.base05),
+            focus: Style::default().fg(self.black).bg(self.green),
+            select: Style::default().fg(self.black).bg(self.base0e),
+            cursor: None,
+            ..TextInputStyle::default()
+        }
+    }
+
+    pub fn input_mask_style(&self) -> MaskedInputStyle {
+        MaskedInputStyle {
+            style: Style::default().fg(self.black).bg(self.base05),
+            focus: Style::default().fg(self.black).bg(self.green),
+            select: Style::default().fg(self.black).bg(self.base0e),
+            cursor: None,
+            invalid: Some(Style::default().fg(Color::White).bg(Color::Red)),
+            ..MaskedInputStyle::default()
+        }
+    }
+
+    pub fn button_style(&self) -> ButtonStyle {
+        ButtonStyle {
+            style: Style::default().fg(self.black).bg(self.purple).bold(),
+            focus: Style::default().fg(self.black).bg(self.green).bold(),
+            armed: Style::default().fg(self.black).bg(self.orange).bold(),
+        }
+    }
+
+    pub fn status_dialog_style(&self) -> StatusDialogStyle {
+        StatusDialogStyle {
+            style: self.status_style(),
+            button: self.button_style(),
+        }
+    }
+
+    pub fn menu_style(&self) -> MenuStyle {
+        MenuStyle {
+            style: Style::default().fg(self.white).bg(self.one_bg3).bold(),
+            title: Style::default().fg(self.black).bg(self.base0a).bold(),
+            select: Style::default().fg(self.black).bg(self.base0e).bold(),
+            focus: Style::default().fg(self.black).bg(self.green).bold(),
+        }
+    }
+}
+
+pub static ONEDARK: Theme = Theme {
+    name: "onedark",
+    dark_theme: false,
+
+    white: Color::from_u32(0xabb2bf),
+    darker_black: Color::from_u32(0x1b1f27),
+    black: Color::from_u32(0x1e222a), //  nvim bg
+    black2: Color::from_u32(0x252931),
+    one_bg: Color::from_u32(0x282c34), // real bg of onedark
+    one_bg2: Color::from_u32(0x353b45),
+    one_bg3: Color::from_u32(0x373b43),
+    grey: Color::from_u32(0x42464e),
+    grey_fg: Color::from_u32(0x565c64),
+    grey_fg2: Color::from_u32(0x6f737b),
+    light_grey: Color::from_u32(0x6f737b),
+    red: Color::from_u32(0xe06c75),
+    baby_pink: Color::from_u32(0xDE8C92),
+    pink: Color::from_u32(0xff75a0),
+    line: Color::from_u32(0x31353d), // for lines like vertsplit
+    green: Color::from_u32(0x98c379),
+    vibrant_green: Color::from_u32(0x7eca9c),
+    nord_blue: Color::from_u32(0x81A1C1),
+    blue: Color::from_u32(0x61afef),
+    yellow: Color::from_u32(0xe7c787),
+    sun: Color::from_u32(0xEBCB8B),
+    purple: Color::from_u32(0xde98fd),
+    dark_purple: Color::from_u32(0xc882e7),
+    teal: Color::from_u32(0x519ABA),
+    orange: Color::from_u32(0xfca2aa),
+    cyan: Color::from_u32(0xa3b8ef),
+    statusline_bg: Color::from_u32(0x22262e),
+    lightbg: Color::from_u32(0x2d3139),
+    pmenu_bg: Color::from_u32(0x61afef),
+    folder_bg: Color::from_u32(0x61afef),
+
+    base00: Color::from_u32(0x1e222a),
+    base01: Color::from_u32(0x353b45),
+    base02: Color::from_u32(0x3e4451),
+    base03: Color::from_u32(0x545862),
+    base04: Color::from_u32(0x565c64),
+    base05: Color::from_u32(0xabb2bf),
+    base06: Color::from_u32(0xb6bdca),
+    base07: Color::from_u32(0xc8ccd4),
+    base08: Color::from_u32(0xe06c75),
+    base09: Color::from_u32(0xd19a66),
+    base0a: Color::from_u32(0xe5c07b),
+    base0b: Color::from_u32(0x98c379),
+    base0c: Color::from_u32(0x56b6c2),
+    base0d: Color::from_u32(0x61afef),
+    base0e: Color::from_u32(0xc678dd),
+    base0f: Color::from_u32(0xbe5046),
+};
+
+fn setup_logging() -> Result<(), anyhow::Error> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {} {}]\n        {}",
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(fern::log_file("log.log")?)
+        .apply()?;
+    Ok(())
+}
