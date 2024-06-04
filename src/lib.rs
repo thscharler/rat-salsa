ce1966ef41590d1b8438b18d98ce84a7117ed085
thscharler <thscharler@gmail.com>
2024-06-04 18:30:36 +0200

    refactor: move flow from rat-salsa up to rat-event. add flow_ok for a Ok-wrapped variant. This should cover all the control-flow constructs.

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@ use std::{ptr, vec};
 pub mod event {
     //! Rexported eventhandling traits.
     pub use rat_event::{
-        crossterm, ct_event, util, ConsumedEvent, FocusKeys, HandleEvent, MouseOnly, Outcome,
+        crossterm, ct_event, flow, flow_ok, util, ConsumedEvent, FocusKeys, HandleEvent, MouseOnly,
+        Outcome,
     };
 }
 
