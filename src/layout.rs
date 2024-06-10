782ee07ad1afdcce43d9268cb03f2f48ae1fa23d
thscharler <thscharler@gmail.com>
2024-06-10 04:55:36 +0200

    refactor: drop old rat-salsa

--- src/layout.rs
+++
@@ -1,5 +0,0 @@
-//!
-//! Some helper functions that can calculate special layouts.
-//!
-pub use rat_input::layout_dialog::{layout_dialog, LayoutDialog};
-pub use rat_input::layout_edit::{layout_edit, EditConstraint, LayoutEdit, LayoutEditIterator};
--- src/widget/calendar.rs
+++
@@ -1,1 +0,0 @@
-pub use rat_input::calendar::{Month, MonthState, MonthStyle};
