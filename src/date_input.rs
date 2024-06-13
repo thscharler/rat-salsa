0d3fd3f2316b24bc214355e005589354fa1e5f88
thscharler <thscharler@gmail.com>
2024-06-13 07:14:13 +0200

    fix: date_input and number_input should provide all functions.

--- src/date_input.rs
+++ src/date_input.rs
@@ -11,6 +11,7 @@ use ratatui::layout::Rect;
 use ratatui::prelude::Style;
 use ratatui::widgets::{Block, StatefulWidget};
 use std::fmt;
+use std::ops::Range;
 
 use crate::event::{FocusKeys, HandleEvent, MouseOnly};
 pub use rat_input::date_input::ConvenientKeys;
@@ -130,12 +131,6 @@ impl RDateInputState {
         })
     }
 
-    /// Reset to empty.
-    #[inline]
-    pub fn clear(&mut self) {
-        self.widget.clear();
-    }
-
     /// chrono format string.
     #[inline]
     pub fn format(&self) -> &str {
@@ -170,6 +165,54 @@ impl RDateInputState {
         self.widget.set_format_loc(pattern, locale)
     }
 
+    /// Reset to empty.
+    #[inline]
+    pub fn clear(&mut self) {
+        self.widget.clear();
+    }
+
+    /// Offset shown.
+    #[inline]
+    pub fn offset(&self) -> usize {
+        self.widget.offset()
+    }
+
+    /// Offset shown. This is corrected if the cursor wouldn't be visible.
+    #[inline]
+    pub fn set_offset(&mut self, offset: usize) {
+        self.widget.set_offset(offset)
+    }
+
+    /// Cursor position
+    #[inline]
+    pub fn cursor(&self) -> usize {
+        self.widget.cursor()
+    }
+
+    /// Set the cursor position, reset selection.
+    #[inline]
+    pub fn set_cursor(&mut self, cursor: usize, extend_selection: bool) -> bool {
+        self.widget.set_cursor(cursor, extend_selection)
+    }
+
+    /// Place cursor at decimal separator, if any. 0 otherwise.
+    #[inline]
+    pub fn set_default_cursor(&mut self) {
+        self.widget.set_default_cursor()
+    }
+
+    /// Selection anchor.
+    #[inline]
+    pub fn anchor(&self) -> usize {
+        self.widget.anchor()
+    }
+
+    /// Create a default value according to the mask.
+    #[inline]
+    pub fn default_value(&self) -> String {
+        self.widget.default_value()
+    }
+
     #[inline]
     pub fn value(&self) -> Result<NaiveDate, chrono::ParseError> {
         self.widget.value()
@@ -180,11 +223,139 @@ impl RDateInputState {
         self.widget.set_value(date)
     }
 
+    /// Empty
     #[inline]
-    pub fn select_all(&mut self) {
-        self.widget.select_all()
+    pub fn is_empty(&self) -> bool {
+        self.widget.is_empty()
     }
 
+    /// Length in grapheme count.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.widget.len()
+    }
+
+    /// Selection
+    #[inline]
+    pub fn has_selection(&self) -> bool {
+        self.widget.has_selection()
+    }
+
+    /// Selection
+    #[inline]
+    pub fn selection(&self) -> Range<usize> {
+        self.widget.selection()
+    }
+
+    /// Selection
+    #[inline]
+    pub fn set_selection(&mut self, anchor: usize, cursor: usize) -> bool {
+        self.widget.set_selection(anchor, cursor)
+    }
+
+    /// Select all text.
+    #[inline]
+    pub fn select_all(&mut self) {
+        self.widget.select_all();
+    }
+
+    /// Selection
+    #[inline]
+    pub fn selected_value(&self) -> &str {
+        self.widget.selected_value()
+    }
+
+    /// Insert a char at the current position.
+    #[inline]
+    pub fn insert_char(&mut self, c: char) -> bool {
+        self.widget.insert_char(c)
+    }
+
+    /// Remove the selected range. The text will be replaced with the default value
+    /// as defined by the mask.
+    #[inline]
+    pub fn delete_range(&mut self, range: Range<usize>) -> bool {
+        self.widget.delete_range(range)
+    }
+
+    /// Deletes the next word.
+    #[inline]
+    pub fn delete_next_word(&mut self) -> bool {
+        self.widget.delete_next_word()
+    }
+
+    /// Deletes the given range.
+    #[inline]
+    pub fn delete_prev_word(&mut self) -> bool {
+        self.widget.delete_prev_word()
+    }
+
+    /// Delete the char before the cursor.
+    #[inline]
+    pub fn delete_prev_char(&mut self) -> bool {
+        self.widget.delete_prev_char()
+    }
+
+    /// Delete the char after the cursor.
+    #[inline]
+    pub fn delete_next_char(&mut self) -> bool {
+        self.widget.delete_next_char()
+    }
+
+    #[inline]
+    pub fn move_to_next_word(&mut self, extend_selection: bool) -> bool {
+        self.widget.move_to_next_word(extend_selection)
+    }
+
+    #[inline]
+    pub fn move_to_prev_word(&mut self, extend_selection: bool) -> bool {
+        self.widget.move_to_next_word(extend_selection)
+    }
+    /// Move to the next char.
+    #[inline]
+    pub fn move_to_next(&mut self, extend_selection: bool) -> bool {
+        self.widget.move_to_next(extend_selection)
+    }
+
+    /// Move to the previous char.
+    #[inline]
+    pub fn move_to_prev(&mut self, extend_selection: bool) -> bool {
+        self.widget.move_to_prev(extend_selection)
+    }
+
+    /// Start of line
+    #[inline]
+    pub fn move_to_line_start(&mut self, extend_selection: bool) -> bool {
+        self.widget.move_to_line_start(extend_selection)
+    }
+
+    /// End of line
+    #[inline]
+    pub fn move_to_line_end(&mut self, extend_selection: bool) -> bool {
+        self.widget.move_to_line_end(extend_selection)
+    }
+
+    /// Converts a grapheme based position to a screen position
+    /// relative to the widget area.
+    pub fn to_screen_col(&self, pos: usize) -> Option<u16> {
+        self.widget.to_screen_col(pos)
+    }
+
+    /// Converts from a widget relative screen coordinate to a grapheme index.
+    /// x is the relative screen position.
+    pub fn from_screen_col(&self, x: usize) -> Option<usize> {
+        self.widget.from_screen_col(x)
+    }
+
+    /// Set the cursor position from a screen position relative to the origin
+    /// of the widget. This value can be negative, which selects a currently
+    /// not visible position and scrolls to it.
+    #[inline]
+    pub fn set_screen_cursor(&mut self, cursor: isize, extend_selection: bool) -> bool {
+        self.widget.set_screen_cursor(cursor, extend_selection)
+    }
+
+    /// Screen position of the cursor for rendering.
     #[inline]
     pub fn screen_cursor(&self) -> Option<(u16, u16)> {
         if self.is_focused() {
