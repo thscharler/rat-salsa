3dc64d29cd2d643709d26acda7fc667805e20f67
thscharler <thscharler@gmail.com>
2024-06-23 16:56:38 +0200

    fix: sync with MenuLine

--- src/menuline.rs
+++ src/menuline.rs
@@ -1,9 +1,15 @@
 //!
-//! A simple menu. No submenus.
+//! Draws a simple line menu.
 //!
-//! Supports hot-keys with '_' in the item text.
+//! If the render area has more than one line, this will
+//! linebreak if necessary.
 //!
-
+//! ## Navigation keys
+//! If you give plain-text strings as items, the underscore
+//! designates a navigation key. If you hit the key, the matching
+//! item is selected. On the second hit, the matching item is
+//! activated.
+//!
 use crate::_private::NonExhaustive;
 use crate::event::{FocusKeys, HandleEvent, MouseOnly};
 #[allow(unused_imports)]
@@ -11,10 +17,11 @@ use log::debug;
 use rat_focus::{FocusFlag, HasFocusFlag};
 use ratatui::buffer::Buffer;
 use ratatui::layout::Rect;
+use ratatui::prelude::Line;
 use ratatui::style::Style;
 use ratatui::widgets::{StatefulWidget, StatefulWidgetRef};
 
-pub use rat_input::menuline::{HotKeyAlt, HotKeyCtrl, MenuOutcome, MenuStyle};
+pub use rat_input::menuline::{MenuOutcome, MenuStyle};
 
 ///
 /// Menu widget.
@@ -33,6 +40,7 @@ pub struct RMenuLine<'a> {
 pub struct RMenuLineState {
     /// State of the inner widget.
     pub widget: rat_input::menuline::MenuLineState,
+
     pub non_exhaustive: NonExhaustive,
 }
 
@@ -40,6 +48,28 @@ impl<'a> RMenuLine<'a> {
     /// New
     pub fn new() -> Self {
         Default::default()
+    }
+
+    /// Title text.
+    #[inline]
+    pub fn title(mut self, title: impl Into<Line<'a>>) -> Self {
+        self.widget = self.widget.title(title);
+        self
+    }
+
+    /// Add a formatted item.
+    /// The navchar is optional, any markup for it is your problem.
+    pub fn add(mut self, item: Line<'a>, navchar: Option<char>) -> Self {
+        self.widget = self.widget.add(item, navchar);
+        self
+    }
+
+    /// Add item.
+    #[inline]
+    #[allow(clippy::should_implement_trait)]
+    pub fn add_str(mut self, menu_item: &'a str) -> Self {
+        self.widget = self.widget.add_str(menu_item);
+        self
     }
 
     /// Combined style.
@@ -72,23 +102,8 @@ impl<'a> RMenuLine<'a> {
 
     /// Selection + Focus
     #[inline]
-    pub fn select_style_focus(mut self, style: impl Into<Style>) -> Self {
-        self.widget = self.widget.select_style_focus(style);
-        self
-    }
-
-    /// Title text.
-    #[inline]
-    pub fn title(mut self, title: &'a str) -> Self {
-        self.widget = self.widget.title(title);
-        self
-    }
-
-    /// Add item.
-    #[inline]
-    #[allow(clippy::should_implement_trait)]
-    pub fn add(mut self, menu_item: &'a str) -> Self {
-        self.widget = self.widget.add(menu_item);
+    pub fn focus_style(mut self, style: impl Into<Style>) -> Self {
+        self.widget = self.widget.focus_style(style);
         self
     }
 }
@@ -131,10 +146,9 @@ impl RMenuLineState {
         self.widget.len()
     }
 
-    /// Selected index
-    #[inline]
-    pub fn selected(&self) -> Option<usize> {
-        self.widget.selected
+    /// Any items.
+    pub fn is_empty(&self) -> bool {
+        self.widget.is_empty()
     }
 
     /// Select
@@ -143,16 +157,16 @@ impl RMenuLineState {
         self.widget.select(select)
     }
 
-    /// Select by hotkey
+    /// Selected index
     #[inline]
-    pub fn select_by_key(&mut self, cc: char) -> bool {
-        self.widget.select_by_key(cc)
+    pub fn selected(&self) -> Option<usize> {
+        self.widget.selected
     }
 
-    /// Item at position.
+    /// Previous item.
     #[inline]
-    pub fn item_at(&self, pos: (u16, u16)) -> Option<usize> {
-        self.widget.item_at(pos)
+    pub fn prev(&mut self) -> bool {
+        self.widget.prev()
     }
 
     /// Next item.
@@ -162,10 +176,22 @@ impl RMenuLineState {
         self.widget.next()
     }
 
-    /// Previous item.
+    /// Select by hotkey
     #[inline]
-    pub fn prev(&mut self) -> bool {
-        self.widget.prev()
+    pub fn navigate(&mut self, c: char) -> MenuOutcome {
+        self.widget.navigate(c)
+    }
+
+    /// Select item at position
+    #[inline]
+    pub fn select_at(&mut self, pos: (u16, u16)) -> bool {
+        self.widget.select_at(pos)
+    }
+
+    /// Item at position.
+    #[inline]
+    pub fn item_at(&self, pos: (u16, u16)) -> Option<usize> {
+        self.widget.item_at(pos)
     }
 }
 
@@ -178,18 +204,6 @@ impl HasFocusFlag for RMenuLineState {
     /// Focus area.
     fn area(&self) -> Rect {
         self.widget.area
-    }
-}
-
-impl HandleEvent<crossterm::event::Event, HotKeyCtrl, MenuOutcome> for RMenuLineState {
-    fn handle(&mut self, event: &crossterm::event::Event, _keymap: HotKeyCtrl) -> MenuOutcome {
-        self.widget.handle(event, HotKeyCtrl)
-    }
-}
-
-impl HandleEvent<crossterm::event::Event, HotKeyAlt, MenuOutcome> for RMenuLineState {
-    fn handle(&mut self, event: &crossterm::event::Event, _keymap: HotKeyAlt) -> MenuOutcome {
-        self.widget.handle(event, HotKeyAlt)
     }
 }
 
