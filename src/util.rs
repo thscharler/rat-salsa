fc97c7983725009015f14aacd7622fcc7e2df0c6
thscharler <thscharler@gmail.com>
2024-08-12 07:57:56 +0200

    refactor:  rename *at_clicked to plan *at. Add those fn to the MouseFlags.

--- src/util.rs
+++ src/util.rs
@@ -11,7 +11,7 @@ use std::sync::atomic::{AtomicU32, Ordering};
 use std::time::SystemTime;
 
 /// Which of the given rects is at the position.
-pub fn item_at_clicked(areas: &[Rect], x_pos: u16, y_pos: u16) -> Option<usize> {
+fn item_at(areas: &[Rect], x_pos: u16, y_pos: u16) -> Option<usize> {
     for (i, r) in areas.iter().enumerate() {
         if y_pos >= r.top() && y_pos < r.bottom() && x_pos >= r.left() && x_pos < r.right() {
             return Some(i);
@@ -25,7 +25,7 @@ pub fn item_at_clicked(areas: &[Rect], x_pos: u16, y_pos: u16) -> Option<usize> 
 ///
 /// You might want to limit calling this functions when the full
 /// position is inside your target rect.
-pub fn row_at_clicked(areas: &[Rect], y_pos: u16) -> Option<usize> {
+fn row_at(areas: &[Rect], y_pos: u16) -> Option<usize> {
     for (i, r) in areas.iter().enumerate() {
         if y_pos >= r.top() && y_pos < r.bottom() {
             return Some(i);
@@ -39,7 +39,7 @@ pub fn row_at_clicked(areas: &[Rect], y_pos: u16) -> Option<usize> {
 ///
 /// You might want to limit calling this functions when the full
 /// position is inside your target rect.
-pub fn column_at_clicked(areas: &[Rect], x_pos: u16) -> Option<usize> {
+fn column_at(areas: &[Rect], x_pos: u16) -> Option<usize> {
     for (i, r) in areas.iter().enumerate() {
         if x_pos >= r.left() && x_pos < r.right() {
             return Some(i);
@@ -54,8 +54,8 @@ pub fn column_at_clicked(areas: &[Rect], x_pos: u16) -> Option<usize> {
 /// sake.
 ///
 /// Rows outside the bounds are returned as Err(isize), rows inside as Ok(usize).
-pub fn row_at_drag(encompassing: Rect, areas: &[Rect], y_pos: u16) -> Result<usize, isize> {
-    if let Some(row) = row_at_clicked(areas, y_pos) {
+fn row_at_drag(encompassing: Rect, areas: &[Rect], y_pos: u16) -> Result<usize, isize> {
+    if let Some(row) = row_at(areas, y_pos) {
         return Ok(row);
     }
 
@@ -78,8 +78,8 @@ pub fn row_at_drag(encompassing: Rect, areas: &[Rect], y_pos: u16) -> Result<usi
 /// sake.
 ///
 /// Columns outside the bounds are returned as Err(isize), rows inside as Ok(usize).
-pub fn column_at_drag(encompassing: Rect, areas: &[Rect], x_pos: u16) -> Result<usize, isize> {
-    if let Some(column) = column_at_clicked(areas, x_pos) {
+fn column_at_drag(encompassing: Rect, areas: &[Rect], x_pos: u16) -> Result<usize, isize> {
+    if let Some(column) = column_at(areas, x_pos) {
         return Ok(column);
     }
 
@@ -112,17 +112,94 @@ pub enum Clicks {
 /// Add this to your widget state.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct MouseFlags {
+    /// Timestamp for double click
     pub time: Cell<Option<SystemTime>>,
     /// Flag for the first down.
     pub click: Cell<Clicks>,
     /// Drag enabled.
     pub drag: Cell<bool>,
+    /// Hover detect.
+    pub hover: Cell<bool>,
 }
 
 impl MouseFlags {
     /// Returns column/row extracted from the Mouse-Event.
     pub fn pos_of(&self, event: &MouseEvent) -> (u16, u16) {
         (event.column, event.row)
+    }
+
+    /// Which of the given rects is at the position.
+    pub fn item_at(&self, areas: &[Rect], x_pos: u16, y_pos: u16) -> Option<usize> {
+        item_at(areas, x_pos, y_pos)
+    }
+
+    /// Which row of the given contains the position.
+    /// This uses only the vertical components of the given areas.
+    ///
+    /// You might want to limit calling this functions when the full
+    /// position is inside your target rect.
+    pub fn row_at(&self, areas: &[Rect], y_pos: u16) -> Option<usize> {
+        row_at(areas, y_pos)
+    }
+
+    /// Column at given position.
+    /// This uses only the horizontal components of the given areas.
+    ///
+    /// You might want to limit calling this functions when the full
+    /// position is inside your target rect.
+    pub fn column_at(&self, areas: &[Rect], x_pos: u16) -> Option<usize> {
+        column_at(areas, x_pos)
+    }
+
+    /// Find a row position when dragging with the mouse. This uses positions
+    /// outside the given areas to estimate an invisible row that could be meant
+    /// by the mouse position. It uses the heuristic `1 row == 1 item` for simplicity’s
+    /// sake.
+    ///
+    /// Rows outside the bounds are returned as Err(isize), rows inside as Ok(usize).
+    pub fn row_at_drag(
+        &self,
+        encompassing: Rect,
+        areas: &[Rect],
+        y_pos: u16,
+    ) -> Result<usize, isize> {
+        row_at_drag(encompassing, areas, y_pos)
+    }
+
+    /// Find a column position when dragging with the mouse. This uses positions
+    /// outside the given areas to estimate an invisible column that could be meant
+    /// by the mouse position. It uses the heuristic `1 column == 1 item` for simplicity’s
+    /// sake.
+    ///
+    /// Columns outside the bounds are returned as Err(isize), rows inside as Ok(usize).
+    pub fn column_at_drag(
+        &self,
+        encompassing: Rect,
+        areas: &[Rect],
+        x_pos: u16,
+    ) -> Result<usize, isize> {
+        column_at_drag(encompassing, areas, x_pos)
+    }
+
+    /// Checks if this is a hover event for the widget.
+    pub fn hover(&self, area: Rect, event: &MouseEvent) -> bool {
+        match event {
+            MouseEvent {
+                kind: MouseEventKind::Moved,
+                column,
+                row,
+                modifiers: KeyModifiers::NONE,
+            } => {
+                let old_hover = self.hover.get();
+                if area.contains((*column, *row).into()) {
+                    self.hover.set(true);
+                } else {
+                    self.hover.set(false);
+                }
+                old_hover != self.hover.get()
+            }
+            _ => false,
+        }
     }
 
     /// Checks if this is a drag event for the widget.
@@ -292,17 +369,94 @@ impl MouseFlags {
 /// Add this to your widget state.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct MouseFlagsN {
+    /// Timestamp for double click
     pub time: Cell<Option<SystemTime>>,
     /// Flag for the first down.
     pub click: Cell<Clicks>,
     /// Drag enabled.
     pub drag: Cell<Option<usize>>,
+    /// Hover detect.
+    pub hover: Cell<Option<usize>>,
 }
 
 impl MouseFlagsN {
     /// Returns column/row extracted from the Mouse-Event.
     pub fn pos_of(&self, event: &MouseEvent) -> (u16, u16) {
         (event.column, event.row)
+    }
+
+    /// Which of the given rects is at the position.
+    pub fn item_at(&self, areas: &[Rect], x_pos: u16, y_pos: u16) -> Option<usize> {
+        item_at(areas, x_pos, y_pos)
+    }
+
+    /// Which row of the given contains the position.
+    /// This uses only the vertical components of the given areas.
+    ///
+    /// You might want to limit calling this functions when the full
+    /// position is inside your target rect.
+    pub fn row_at(&self, areas: &[Rect], y_pos: u16) -> Option<usize> {
+        row_at(areas, y_pos)
+    }
+
+    /// Column at given position.
+    /// This uses only the horizontal components of the given areas.
+    ///
+    /// You might want to limit calling this functions when the full
+    /// position is inside your target rect.
+    pub fn column_at(&self, areas: &[Rect], x_pos: u16) -> Option<usize> {
+        column_at(areas, x_pos)
+    }
+
+    /// Find a row position when dragging with the mouse. This uses positions
+    /// outside the given areas to estimate an invisible row that could be meant
+    /// by the mouse position. It uses the heuristic `1 row == 1 item` for simplicity’s
+    /// sake.
+    ///
+    /// Rows outside the bounds are returned as Err(isize), rows inside as Ok(usize).
+    pub fn row_at_drag(
+        &self,
+        encompassing: Rect,
+        areas: &[Rect],
+        y_pos: u16,
+    ) -> Result<usize, isize> {
+        row_at_drag(encompassing, areas, y_pos)
+    }
+
+    /// Find a column position when dragging with the mouse. This uses positions
+    /// outside the given areas to estimate an invisible column that could be meant
+    /// by the mouse position. It uses the heuristic `1 column == 1 item` for simplicity’s
+    /// sake.
+    ///
+    /// Columns outside the bounds are returned as Err(isize), rows inside as Ok(usize).
+    pub fn column_at_drag(
+        &self,
+        encompassing: Rect,
+        areas: &[Rect],
+        x_pos: u16,
+    ) -> Result<usize, isize> {
+        column_at_drag(encompassing, areas, x_pos)
+    }
+
+    /// Checks if this is a hover event for the widget.
+    pub fn hover(&self, areas: &[Rect], event: &MouseEvent) -> bool {
+        match event {
+            MouseEvent {
+                kind: MouseEventKind::Moved,
+                column,
+                row,
+                modifiers: KeyModifiers::NONE,
+            } => {
+                let old_hover = self.hover.get();
+                if let Some(n) = self.item_at(areas, *column, *row) {
+                    self.hover.set(Some(n));
+                } else {
+                    self.hover.set(None);
+                }
+                old_hover != self.hover.get()
+            }
+            _ => false,
+        }
     }
 
     /// Checks if this is a drag event for the widget.
