1279c4cc4487a9b061d54918a9e60c2956701cb7
thscharler <thscharler@gmail.com>
2024-10-17 21:27:36 +0200

    break: style names changed.

--- src/dark_theme.rs
+++ src/dark_theme.rs
@@ -203,7 +203,7 @@ impl DarkTheme {
     pub fn line_nr_style(&self) -> LineNumberStyle {
         LineNumberStyle {
             style: self.data().fg(self.s.gray[0]),
-            cursor_style: Some(self.text_select()),
+            cursor: Some(self.text_select()),
             ..LineNumberStyle::default()
         }
     }
@@ -252,7 +252,7 @@ impl DarkTheme {
     pub fn table_style(&self) -> TableStyle {
         TableStyle {
             style: self.data(),
-            select_row_style: Some(self.select()),
+            select_row: Some(self.select()),
             show_row_focus: true,
             focus_style: Some(self.focus()),
             ..Default::default()
@@ -263,8 +263,8 @@ impl DarkTheme {
     pub fn list_style(&self) -> ListStyle {
         ListStyle {
             style: self.data(),
-            select_style: Some(self.select()),
-            focus_style: Some(self.focus()),
+            select: Some(self.select()),
+            focus: Some(self.focus()),
             ..Default::default()
         }
     }
@@ -316,9 +316,9 @@ impl DarkTheme {
         let style = Style::default().fg(self.s.gray[0]).bg(self.s.black[1]);
         TabbedStyle {
             style,
-            tab_style: Some(self.gray(1)),
-            select_style: Some(self.gray(3)),
-            focus_style: Some(self.focus()),
+            tab: Some(self.gray(1)),
+            select: Some(self.gray(3)),
+            focus: Some(self.focus()),
             ..Default::default()
         }
     }
