3125848eff1aa59de472e6e20427fc6ca4177c09
thscharler <thscharler@gmail.com>
2024-12-27 19:19:16 +0100

    fix: test

--- tests/test_layout_form.rs
+++ tests/test_layout_form.rs
@@ -33,7 +33,7 @@ fn test_break2() {
     let g = layout.layout(Size::new(10, 5), Padding::new(0, 0, 1, 1));
 
     assert_eq!(g.page_of(&4), Some(1));
-    assert_eq!(g.page_of(&7), Some(2));
+    assert_eq!(g.page_of(&7), Some(3));
 }
 
 #[test]
@@ -54,16 +54,16 @@ fn test_break3() {
     assert_eq!(g.page_of(&2), Some(1));
     assert_eq!(g.page_of(&3), Some(2));
     assert_eq!(g.page_of(&4), Some(2));
-    assert_eq!(g.page_of(&5), Some(2));
+    assert_eq!(g.page_of(&5), Some(3));
     assert_eq!(g.page_of(&6), Some(3));
-    assert_eq!(g.page_of(&7), Some(3));
+    assert_eq!(g.page_of(&7), Some(4));
 }
 
 #[test]
 fn test_break4() {
     let mut layout = LayoutForm::<i32>::new();
 
-    layout.start((), Some(Block::bordered()));
+    let tag = layout.start(Some(Block::bordered()));
     layout.widget(1, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(2, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(3, FormLabel::Width(5), FormWidget::Width(15));
@@ -71,31 +71,30 @@ fn test_break4() {
     layout.widget(5, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(6, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(7, FormLabel::Width(5), FormWidget::Width(15));
-    layout.end(());
+    layout.end(tag);
 
     let g = layout.layout(Size::new(10, 5), Padding::new(0, 0, 1, 1));
-    assert_eq!(g.page_of(&7), Some(6));
-    assert_eq!(g.container_areas[6], Rect::new(0, 31, 10, 3));
+    assert_eq!(g.page_of(&7), Some(7));
+    assert_eq!(g.block_area(6), Rect::new(0, 36, 10, 3));
 }
 
 #[test]
 fn test_break5() {
     let mut layout = LayoutForm::<i32>::new();
 
-    layout.start((), Some(Block::bordered()));
-    layout.start((), Some(Block::bordered()));
+    let tag1 = layout.start(Some(Block::bordered()));
+    let tag2 = layout.start(Some(Block::bordered()));
     layout.widget(1, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(2, FormLabel::Width(5), FormWidget::Width(15));
-    layout.end(());
+    layout.end(tag2);
     layout.widget(3, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(4, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(5, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(6, FormLabel::Width(5), FormWidget::Width(15));
     layout.widget(7, FormLabel::Width(5), FormWidget::Width(15));
-    layout.end(());
+    layout.end(tag1);
 
     let g = layout.layout(Size::new(10, 14), Padding::new(0, 0, 1, 1));
-    dbg!(&g);
-    assert_eq!(g.page_of(&7), Some(6));
-    assert_eq!(g.container_areas[6], Rect::new(0, 31, 10, 3));
+    assert_eq!(g.page_of(&7), Some(0));
+    assert_eq!(g.block_area(1), Rect::new(0, 1, 10, 11));
 }
