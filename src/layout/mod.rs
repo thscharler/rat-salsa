4052a1a6c9534cfe1f8d72cad6c8d05acd956cbc
thscharler <thscharler@gmail.com>
2024-12-28 18:57:25 +0100

    refactor: remove duplicates, rename stuff

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -1,19 +1,15 @@
 mod generic_layout;
 mod layout_dialog;
-mod layout_dialog2;
-mod layout_edit2;
+mod layout_edit;
 mod layout_form;
 mod layout_grid;
-mod layout_grid2;
 mod layout_middle;
 mod structured_layout;
 
 pub use generic_layout::GenericLayout;
-pub use layout_dialog::{layout_dialog as xx_layout_dialog, DialogItem as XXDialogItem};
-pub use layout_dialog2::{layout_dialog, DialogItem};
-pub use layout_edit2::{layout_edit, EditConstraint};
+pub use layout_dialog::{layout_dialog, DialogItem};
+pub use layout_edit::{layout_edit, EditConstraint};
 pub use layout_form::{FormLabel, FormWidget, LayoutForm};
-pub use layout_grid::layout_grid as xx_layout_grid;
-pub use layout_grid2::layout_grid;
+pub use layout_grid::layout_grid;
 pub use layout_middle::layout_middle;
 pub use structured_layout::StructuredLayout;
