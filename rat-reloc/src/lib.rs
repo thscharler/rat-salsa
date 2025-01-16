4d2a0403a2af482ffb0e74839db4d4a0d1b8916f
thscharler <thscharler@gmail.com>
2025-01-16 15:42:54 +0100

    feature: implement RelocatableState for Rect. Allows more uses for the impl_relocatable_state macro.

--- rat-reloc/src/lib.rs
+++ rat-reloc/src/lib.rs
@@ -29,6 +29,12 @@ macro_rules! impl_relocatable_state {
     };
 }
 
+impl RelocatableState for Rect {
+    fn relocate(&mut self, shift: (i16, i16), clip: Rect) {
+        *self = relocate_area(*self, shift, clip);
+    }
+}
+
 /// Shift the area by offset and clip it.
 pub fn relocate_areas(area: &mut [Rect], shift: (i16, i16), clip: Rect) {
     for a in area {
