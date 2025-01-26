cd80ecadc595fb5e24e8ecd114eeff8b6fbf5ee1
thscharler <thscharler@gmail.com>
2025-01-26 20:13:52 +0100

    feature: add try_label_str_for() and try_label_str() for optional results. default returns "" as default for missing labels. feature: add new widget Form that further reduces the surface at the price of loosing functionality.

---
+++ rat-widget/examples/form1.rs
@@ -0,0 +1,174 @@
+#![allow(dead_code)]
+
+use crate::mini_salsa::text_input_mock::{TextInputMock, TextInputMockState};
+use crate::mini_salsa::theme::THEME;
+use crate::mini_salsa::{run_ui, setup_logging, MiniSalsaState};
+use rat_event::{HandleEvent, Regular};
+use rat_focus::{Focus, FocusBuilder, FocusFlag};
+use rat_menu::event::MenuOutcome;
+use rat_menu::menuline::{MenuLine, MenuLineState};
+use rat_text::HasScreenCursor;
+use rat_widget::event::Outcome;
+use rat_widget::layout::{FormLabel, FormWidget, LayoutForm};
+use rat_widget::pager::{Form, FormState};
+use ratatui::layout::{Constraint, Flex, Layout, Rect};
+use ratatui::text::Span;
+use ratatui::widgets::Padding;
+use ratatui::Frame;
+use std::array;
+use std::cmp::max;
+
+mod mini_salsa;
+
+const HUN: usize = 100;
+
+fn main() -> Result<(), anyhow::Error> {
+    setup_logging()?;
+
+    let mut data = Data {};
+
+    let mut state = State {
+        form: FormState::default(),
+        hundred: array::from_fn(|_| Default::default()),
+        menu: Default::default(),
+    };
+    state.menu.focus.set(true);
+    state.menu.select(Some(0));
+
+    run_ui("pager1", handle_input, repaint_input, &mut data, &mut state)
+}
+
+struct Data {}
+
+struct State {
+    form: FormState<FocusFlag>,
+    hundred: [TextInputMockState; HUN],
+    menu: MenuLineState,
+}
+
+fn repaint_input(
+    frame: &mut Frame<'_>,
+    area: Rect,
+    _data: &mut Data,
+    _istate: &mut MiniSalsaState,
+    state: &mut State,
+) -> Result<(), anyhow::Error> {
+    let l1 = Layout::vertical([
+        Constraint::Length(1),
+        Constraint::Fill(1),
+        Constraint::Length(1),
+        Constraint::Length(1),
+    ])
+    .split(area);
+
+    let l2 = Layout::horizontal([
+        Constraint::Length(3),
+        Constraint::Fill(1),
+        Constraint::Length(3),
+    ])
+    .split(l1[1]);
+
+    // set up pager
+    let form = Form::new() //
+        .styles(THEME.pager_style());
+
+    // maybe rebuild layout
+    let layout_size = form.layout_size(l2[1]);
+
+    if !state.form.valid_layout(layout_size) {
+        let mut form = LayoutForm::new() //
+            .spacing(1)
+            .line_spacing(1)
+            .flex(Flex::Legacy);
+        for i in 0..state.hundred.len() {
+            let h = if i % 3 == 0 {
+                2
+            } else if i % 5 == 0 {
+                5
+            } else {
+                1
+            };
+
+            form.widget(
+                state.hundred[i].focus.clone(),
+                FormLabel::Width(5),
+                FormWidget::Size(15, h),
+            );
+            if i == 17 {
+                form.page_break();
+            }
+        }
+
+        state
+            .form
+            .set_layout(form.paged(layout_size, Padding::new(2, 2, 1, 1)));
+    }
+
+    // set current layout and prepare rendering.
+    let mut pager = form.into_buffer(l2[1], frame.buffer_mut(), &mut state.form);
+
+    // render the input fields.
+    for i in 0..state.hundred.len() {
+        // render manual label
+        pager.render_label(
+            state.hundred[i].focus.clone(), //
+            |_| Span::from("<<?>>"),
+        );
+
+        // map our widget area.
+        pager.render(
+            state.hundred[i].focus.clone(),
+            || {
+                TextInputMock::default()
+                    .sample(format!("{:?}", i))
+                    .style(THEME.limegreen(0))
+                    .focus_style(THEME.limegreen(2))
+            },
+            &mut state.hundred[i],
+        );
+    }
+
+    let menu1 = MenuLine::new()
+        .title("#.#")
+        .item_parsed("_Quit")
+        .styles(THEME.menu_style());
+    frame.render_stateful_widget(menu1, l1[3], &mut state.menu);
+
+    for i in 0..state.hundred.len() {
+        if let Some(cursor) = state.hundred[i].screen_cursor() {
+            frame.set_cursor_position(cursor);
+        }
+    }
+
+    Ok(())
+}
+
+fn focus(state: &State) -> Focus {
+    let mut fb = FocusBuilder::default();
+    fb.widget(&state.menu);
+    for i in 0..state.hundred.len() {
+        // Focus wants __all__ areas.
+        fb.widget(&state.hundred[i]);
+    }
+    fb.build()
+}
+
+fn handle_input(
+    event: &crossterm::event::Event,
+    _data: &mut Data,
+    istate: &mut MiniSalsaState,
+    state: &mut State,
+) -> Result<Outcome, anyhow::Error> {
+    let mut focus = focus(state);
+    let f = focus.handle(event, Regular);
+
+    let r = match state.menu.handle(event, Regular) {
+        MenuOutcome::Activated(0) => {
+            istate.quit = true;
+            Outcome::Changed
+        }
+        _ => Outcome::Continue,
+    };
+
+    Ok(max(f, r))
+}
