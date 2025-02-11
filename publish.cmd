a2376dfe2d85e1d4fc3506556d5fd8311d441d6b
thscharler <thscharler@gmail.com>
2025-02-11 04:41:31 +0100

    fix: add my publish.cmd

---
+++ publish.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+cargo publish -p rat-cursor
+cargo publish -p rat-event
+cargo publish -p rat-reloc
+cargo publish -p rat-focus
+cargo publish -p rat-scrolled
+
+cargo publish -p rat-popup
+
+cargo publish -p rat-menu
+cargo publish -p rat-text
+cargo publish -p rat-markdown
+cargo publish -p rat-ftable
+cargo publish -p rat-widget
+
+cargo publish -p rat-theme
+cargo publish -p rat-theme2
+
+cargo publish -p rat-salsa
+cargo publish -p rat-dialog
+
