1fba11368e95c13db4260d99371b3e5e6a98be8e
thscharler <thscharler@gmail.com>
2025-09-08 21:36:34 +0200

    fix: cleanup

--- rat-salsa/examples/ultra.rs
+++ rat-salsa/examples/ultra.rs
@@ -1,12 +1,14 @@
 use anyhow::{anyhow, Error};
 use rat_salsa::poll::PollCrossterm;
-use rat_salsa::{mock, run_tui, Control, RunConfig, SalsaAppContext, SalsaContext};
+use rat_salsa::{mock, run_tui, Control, RunConfig, SalsaAppContext};
 use rat_widget::event::ct_event;
 use ratatui::buffer::Buffer;
 use ratatui::layout::Rect;
 use ratatui::style::Stylize;
 use ratatui::text::{Line, Span};
 use ratatui::widgets::Widget;
+
+type Global = SalsaAppContext<UltraEvent, Error>;
 
 fn main() -> Result<(), Error> {
     run_tui(
@@ -15,26 +17,9 @@ fn main() -> Result<(), Error> {
         event,
         error,
         &mut Global::default(),
-        &mut Ultra,
+        &mut Ultra::default(),
         RunConfig::default()?.poll(PollCrossterm),
     )
-}
-
-#[derive(Debug, Default)]
-pub struct Global {
-    ctx: SalsaAppContext<UltraEvent, Error>,
-    pub err_cnt: u32,
-    pub err_msg: String,
-}
-
-impl SalsaContext<UltraEvent, Error> for Global {
-    fn set_salsa_ctx(&mut self, app_ctx: SalsaAppContext<UltraEvent, Error>) {
-        self.ctx = app_ctx;
-    }
-
-    fn salsa_ctx(&self) -> &SalsaAppContext<UltraEvent, Error> {
-        &self.ctx
-    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -48,18 +33,22 @@ impl From<crossterm::event::Event> for UltraEvent {
     }
 }
 
-pub struct Ultra;
+#[derive(Default)]
+pub struct Ultra {
+    pub err_cnt: u32,
+    pub err_msg: String,
+}
 
-fn render(area: Rect, buf: &mut Buffer, _state: &mut Ultra, ctx: &mut Global) -> Result<(), Error> {
+fn render(area: Rect, buf: &mut Buffer, state: &mut Ultra, _ctx: &mut Global) -> Result<(), Error> {
     Line::from_iter([Span::from("'q' to quit, 'e' for error, 'r' for repair")])
         .render(Rect::new(area.x, area.y, area.width, 1), buf);
     Line::from_iter([
         Span::from("Hello world!").green(),
         Span::from(" Status: "),
-        if ctx.err_cnt > 0 {
-            Span::from(&ctx.err_msg).red().underlined()
+        if state.err_cnt > 0 {
+            Span::from(&state.err_msg).red().underlined()
         } else {
-            Span::from(&ctx.err_msg).cyan().underlined()
+            Span::from(&state.err_msg).cyan().underlined()
         },
     ])
     .render(Rect::new(area.x, area.y + 2, area.width, 1), buf);
@@ -68,23 +57,23 @@ fn render(area: Rect, buf: &mut Buffer, _state: &mut Ultra, ctx: &mut Global) ->
 
 fn event(
     event: &UltraEvent,
-    _state: &mut Ultra,
-    ctx: &mut Global,
+    state: &mut Ultra,
+    _ctx: &mut Global,
 ) -> Result<Control<UltraEvent>, Error> {
     match event {
         UltraEvent::Event(event) => match event {
             ct_event!(key press 'q') => Ok(Control::Quit),
-            ct_event!(key press 'e') => return Err(anyhow!("An error occured.")),
+            ct_event!(key press 'e') => Err(anyhow!("An error occured.")),
             ct_event!(key press 'r') => {
-                if ctx.err_cnt > 1 {
-                    ctx.err_cnt -= 1;
-                    ctx.err_msg = format!("#{}# One error repaired.", ctx.err_cnt).to_string();
-                } else if ctx.err_cnt == 1 {
-                    ctx.err_cnt -= 1;
-                    ctx.err_msg = "All within norms.".to_string();
+                if state.err_cnt > 1 {
+                    state.err_cnt -= 1;
+                    state.err_msg = format!("#{}# One error repaired.", state.err_cnt).to_string();
+                } else if state.err_cnt == 1 {
+                    state.err_cnt -= 1;
+                    state.err_msg = "All within norms.".to_string();
                 } else {
-                    ctx.err_cnt = 1;
-                    ctx.err_msg = format!("#{}# Over-repaired.", ctx.err_cnt).to_string();
+                    state.err_cnt = 1;
+                    state.err_msg = format!("#{}# Over-repaired.", state.err_cnt).to_string();
                 }
                 Ok(Control::Changed)
             }
@@ -93,8 +82,8 @@ fn event(
     }
 }
 
-fn error(event: Error, _state: &mut Ultra, ctx: &mut Global) -> Result<Control<UltraEvent>, Error> {
-    ctx.err_cnt += 1;
-    ctx.err_msg = format!("#{}# {}", ctx.err_cnt, event).to_string();
+fn error(event: Error, state: &mut Ultra, _ctx: &mut Global) -> Result<Control<UltraEvent>, Error> {
+    state.err_cnt += 1;
+    state.err_msg = format!("#{}# {}", state.err_cnt, event).to_string();
     Ok(Control::Changed)
 }
