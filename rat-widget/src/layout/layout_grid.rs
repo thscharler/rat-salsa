f1c9a055d4e772a6f172cfb759a6d87f1faff96d
thscharler <thscharler@gmail.com>
2025-09-27 21:23:07 +0200

    fix: add layout_as_grid without type parameters.

--- rat-widget/src/layout/layout_grid.rs
+++ rat-widget/src/layout/layout_grid.rs
@@ -29,6 +29,7 @@ use ratatui::layout::{Constraint, Layout, Rect};
 /// let a_1_2 = layout.widget(layout.try_index_of((1,2)).expect("fine"));
 /// ```
 #[allow(clippy::needless_range_loop)]
+#[deprecated(since = "1.2.0", note = "use layout_as_grid() instead")]
 pub fn layout_grid<const X: usize, const Y: usize>(
     area: Rect,
     horizontal: Layout,
@@ -43,6 +44,55 @@ pub fn layout_grid<const X: usize, const Y: usize>(
 
     for x in 0..X {
         for y in 0..Y {
+            let grid_area = Rect::new(hori[x].x, vert[y].y, hori[x].width, vert[y].height);
+            gen_layout.add((x, y), grid_area, None, Rect::default());
+        }
+    }
+
+    gen_layout
+}
+
+///
+/// Calculates a full grid of rects from the horizontal and vertical components.
+///
+/// ```
+/// use ratatui::layout::{Constraint, Layout, Rect};
+/// use rat_widget::layout::layout_as_grid;
+///
+/// let area = Rect::new(0,0,100,100);
+///
+/// let layout = layout_as_grid(area,
+///             Layout::horizontal([
+///                 Constraint::Length(5),
+///                 Constraint::Fill(1),
+///                 Constraint::Length(5)
+///             ]),
+///             Layout::vertical([
+///                 Constraint::Length(1),
+///                 Constraint::Length(3),
+///                 Constraint::Fill(1),
+///                 Constraint::Length(3),
+///                 Constraint::Length(1),
+///             ])
+/// );
+///
+/// // middle column, second block
+/// let a_1_2 = layout.widget(layout.try_index_of((1,2)).expect("fine"));
+/// ```
+pub fn layout_as_grid(
+    area: Rect,
+    horizontal: Layout,
+    vertical: Layout,
+) -> GenericLayout<(usize, usize)> {
+    let mut gen_layout = GenericLayout::new();
+    gen_layout.set_area(area);
+    gen_layout.set_page_size(area.as_size());
+
+    let hori = horizontal.split(Rect::new(area.x, 0, area.width, 0));
+    let vert = vertical.split(Rect::new(0, area.y, 0, area.height));
+
+    for x in 0..hori.len() {
+        for y in 0..vert.len() {
             let grid_area = Rect::new(hori[x].x, vert[y].y, hori[x].width, vert[y].height);
             gen_layout.add((x, y), grid_area, None, Rect::default());
         }
