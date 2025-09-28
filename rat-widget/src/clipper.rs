852a233db6a6c43441b6c4b3eaa9322027bb6649
thscharler <thscharler@gmail.com>
2025-09-28 06:11:06 +0200

    refactor: move clipper

--- rat-widget/src/clipper.rs
+++ rat-widget/src/clipper.rs
@@ -1,11 +1,100 @@
+//!
+//! An alternative view widget.
+//!
+//! > The extra requirement for this one is that you can create
+//! > a Layout that defines the bounds of all widgets that can
+//! > be rendered.
+//!
+//! It works in 4 phases:
+//!
+//! ```rust no_run
+//!     # use rat_widget::clipper::{Clipper, ClipperState};
+//!     # use rat_widget::checkbox::{Checkbox, CheckboxState};
+//!     # use ratatui::prelude::*;
+//!     # use rat_focus::{FocusFlag, HasFocus};
+//!     # use rat_widget::layout::GenericLayout;
+//!     #
+//!     # let l2 = [Rect::ZERO, Rect::ZERO];
+//!     # struct State {
+//!     #      check_states: Vec<CheckboxState>,
+//!     #      clipper: ClipperState<FocusFlag>
+//!     #  }
+//!     # let mut state = State {
+//!     #      clipper: Default::default(),
+//!     #      check_states: Vec::default()
+//!     #  };
+//!     # let mut buf = Buffer::default();
+//!
+//!     /// Create the layout. The layout can be stored long-term
+//!     /// and needs to be rebuilt only if your widget layout changes.
+//!
+//!     let clipper = Clipper::new();
+//!     let layout_size = clipper.layout_size(l2[1], &mut state.clipper);
+//!
+//!     if !state.clipper.valid_layout(layout_size) {
+//!         let mut cl = GenericLayout::new();
+//!         for i in 0..100 {
+//!             cl.add(state.check_states[i].focus(),
+//!                 Rect::new(10, i as u16 *11, 15, 10),
+//!                 None,
+//!                 Rect::default()
+//!             );
+//!         }
+//!         state.clipper.set_layout(cl);
+//!     }
+//!
+//!     /// The given area plus the current scroll offset define the
+//!     /// view area. With the view area a temporary buffer is created
+//!     /// that is big enough to fit all widgets that are at least
+//!     /// partially visible.
+//!
+//!     let mut clip_buf = clipper
+//!         .into_buffer(l2[1], &mut state.clipper);
+//!
+//!     ///
+//!     /// The widgets are rendered to that buffer.
+//!     ///
+//!     for i in 0..100 {
+//!         // refer by handle
+//!         clip_buf.render(
+//!             state.check_states[i].focus(),
+//!             || {
+//!                 Checkbox::new()
+//!                 .text(format!("{:?}", i))
+//!             },
+//!             &mut state.check_states[i],
+//!         );
+//!     }
+//!
+//!     ///
+//!     /// The last step clips and copies the buffer to the frame buffer.
+//!     ///
+//!
+//!     clip_buf
+//!         .into_widget()
+//!         .render(l2[1], &mut buf, &mut state.clipper);
+//!
+//! ```
+//!
+//! __StatefulWidget__
+//!
+//! For this to work with StatefulWidgets they must cooperate
+//! by implementing the [RelocatableState](crate::reloc::RelocatableState)
+//! trait. With this trait the widget can clip/hide all areas that
+//! it stores in its state.
+//!
+//! __See__
+//!
+//! [example](https://github.com/thscharler/rat-widget/blob/master/examples/clipper1.rs)
+//!
+
 use crate::_private::NonExhaustive;
-use crate::clipper::ClipperStyle;
 use crate::layout::GenericLayout;
 use rat_event::{ConsumedEvent, HandleEvent, MouseOnly, Outcome, Regular, ct_event};
 use rat_focus::{FocusBuilder, FocusFlag, HasFocus};
 use rat_reloc::RelocatableState;
 use rat_scrolled::event::ScrollOutcome;
-use rat_scrolled::{Scroll, ScrollArea, ScrollAreaState, ScrollState};
+use rat_scrolled::{Scroll, ScrollArea, ScrollAreaState, ScrollState, ScrollStyle};
 use ratatui::buffer::Buffer;
 use ratatui::layout::{Alignment, Position, Rect, Size};
 use ratatui::style::Style;
@@ -73,6 +162,30 @@ where
     hscroll: Option<Scroll<'a>>,
     vscroll: Option<Scroll<'a>>,
     phantom: PhantomData<W>,
+}
+
+/// Clipper styles.
+#[derive(Debug, Clone)]
+pub struct ClipperStyle {
+    pub style: Style,
+    pub label_style: Option<Style>,
+    pub label_alignment: Option<Alignment>,
+    pub block: Option<Block<'static>>,
+    pub scroll: Option<ScrollStyle>,
+    pub non_exhaustive: NonExhaustive,
+}
+
+impl Default for ClipperStyle {
+    fn default() -> Self {
+        Self {
+            style: Default::default(),
+            label_style: None,
+            label_alignment: None,
+            block: None,
+            scroll: None,
+            non_exhaustive: NonExhaustive,
+        }
+    }
 }
 
 #[derive(Debug)]
