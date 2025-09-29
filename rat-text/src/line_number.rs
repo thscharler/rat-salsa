ec651a804566757a2eeec1da241be80c300e0a4d
thscharler <thscharler@gmail.com>
2025-09-29 06:02:36 +0200

    docs: add

--- rat-text/src/line_number.rs
+++ rat-text/src/line_number.rs
@@ -1,6 +1,32 @@
 //!
 //! Line numbers widget.
 //!
+//!
+//! Render line numbers in sync with a text area.
+//! ```
+//! # use ratatui::buffer::Buffer;
+//! # use ratatui::layout::Rect;
+//! # use ratatui::symbols::border::EMPTY;
+//! # use ratatui::widgets::{Block, Borders, StatefulWidget};
+//! use rat_text::line_number::{LineNumberState, LineNumbers};
+//! # use rat_text::text_area::TextAreaState;
+//!
+//! # struct State {textarea: TextAreaState, line_numbers: LineNumberState}
+//! # let mut state = State {textarea: Default::default(),line_numbers: Default::default()};
+//! # let mut buf = Buffer::default();
+//! # let buf = &mut buf;
+//! # let area = Rect::default();
+//!
+//! LineNumbers::new()
+//!     .block(
+//!         Block::new()
+//!             .borders(Borders::TOP | Borders::BOTTOM)
+//!             .border_set(EMPTY),
+//!     )
+//! .with_textarea(&state.textarea)
+//! .render(area, buf, &mut state.line_numbers);
+//! ```
+//!
 
 use crate::_private::NonExhaustive;
 use crate::text_area::TextAreaState;
@@ -107,6 +133,8 @@ impl<'a> LineNumbers<'a> {
     }
 
     /// Extra info.
+    ///
+    /// This is a Vec that matches up the visible lines.
     pub fn flags(mut self, flags: Vec<Line<'a>>) -> Self {
         self.flags = flags;
         self
