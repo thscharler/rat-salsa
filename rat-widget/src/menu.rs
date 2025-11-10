2c9473cebaa1ed75338096ee225354d0b28c4ed7
thscharler <thscharler@gmail.com>
2025-11-10 06:51:41 +0100

    fix: missing things according to conformance test.

--- rat-widget/src/menu.rs
+++ rat-widget/src/menu.rs
@@ -8,11 +8,11 @@ pub use rat_menu::popup_menu::{PopupConstraint, PopupMenu, PopupMenuState};
 pub use rat_menu::{MenuBuilder, MenuStructure, MenuStyle, StaticMenu};
 
 pub mod menubar {
-    pub use rat_menu::menubar::{handle_mouse_events, handle_popup_events};
+    pub use rat_menu::menubar::{handle_events, handle_mouse_events, handle_popup_events};
 }
 pub mod menuline {
     pub use rat_menu::menuline::{handle_events, handle_mouse_events};
 }
 pub mod popup_menu {
-    pub use rat_menu::popup_menu::{handle_mouse_events, handle_popup_events};
+    pub use rat_menu::popup_menu::{handle_events, handle_mouse_events, handle_popup_events};
 }
