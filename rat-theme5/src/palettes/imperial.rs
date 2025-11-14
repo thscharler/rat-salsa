32965a9a5ea7d98f89ddd0074597122c3ebc4556
thscharler <thscharler@gmail.com>
2025-11-14 21:47:25 +0100

    feat: rename according to rat-theme vesion. and done.

--- rat-theme5/src/palettes/imperial.rs
+++ rat-theme5/src/palettes/imperial.rs
@@ -26,7 +26,7 @@ pub const IMPERIAL: Palette = {
             Palette::interpolate(0x234668, 0x2b81d4, DARKNESS),
             Palette::interpolate(0x202083, 0x3232cc, DARKNESS),
             Palette::interpolate(0x4b0089, 0x8c00fd, DARKNESS),
-            Palette::interpolate(0x4f1b4f, 0xbd42bd, DARKNESS),
+            Palette::interpolate(0x4f1b4f, 0xbd44bd, DARKNESS),
             Palette::interpolate(0x47101d, 0xc33c5b, DARKNESS),
         ],
         color_ext: [Color::Reset; ColorsExt::LEN],
@@ -34,32 +34,32 @@ pub const IMPERIAL: Palette = {
 
     p.color_ext[ColorsExt::LabelFg as usize] = p.color[Colors::White as usize][0];
     p.color_ext[ColorsExt::Input as usize] = p.color[Colors::Gray as usize][3];
-    p.color_ext[ColorsExt::Focus as usize] = p.color[Colors::Primary as usize][2];
+    p.color_ext[ColorsExt::Focus as usize] = p.color[Colors::Primary as usize][1];
     p.color_ext[ColorsExt::Select as usize] = p.color[Colors::Secondary as usize][1];
     p.color_ext[ColorsExt::Disabled as usize] = p.color[Colors::Gray as usize][3];
-    p.color_ext[ColorsExt::Invalid as usize] = p.color[Colors::Red as usize][3];
+    p.color_ext[ColorsExt::Invalid as usize] = p.color[Colors::Red as usize][1];
     p.color_ext[ColorsExt::TitleFg as usize] = p.color[Colors::TextLight as usize][0];
-    p.color_ext[ColorsExt::Title as usize] = p.color[Colors::Orange as usize][3];
+    p.color_ext[ColorsExt::Title as usize] = p.color[Colors::Red as usize][0];
     p.color_ext[ColorsExt::HeaderFg as usize] = p.color[Colors::TextLight as usize][0];
-    p.color_ext[ColorsExt::Header as usize] = p.color[Colors::Blue as usize][1];
+    p.color_ext[ColorsExt::Header as usize] = p.color[Colors::Blue as usize][0];
     p.color_ext[ColorsExt::FooterFg as usize] = p.color[Colors::TextLight as usize][0];
-    p.color_ext[ColorsExt::Footer as usize] = p.color[Colors::Blue as usize][1];
+    p.color_ext[ColorsExt::Footer as usize] = p.color[Colors::Blue as usize][0];
     p.color_ext[ColorsExt::Shadow as usize] = p.color[Colors::TextDark as usize][0];
     p.color_ext[ColorsExt::TextFocus as usize] = p.color[Colors::Primary as usize][1];
     p.color_ext[ColorsExt::TextSelect as usize] = p.color[Colors::Secondary as usize][1];
-    p.color_ext[ColorsExt::ButtonBase as usize] = p.color[Colors::Gray as usize][2];
-    p.color_ext[ColorsExt::MenuBase as usize] = p.color[Colors::TextDark as usize][1];
+    p.color_ext[ColorsExt::ButtonBase as usize] = p.color[Colors::Gray as usize][0];
+    p.color_ext[ColorsExt::MenuBase as usize] = p.color[Colors::Black as usize][1];
     p.color_ext[ColorsExt::KeyBinding as usize] = p.color[Colors::BlueGreen as usize][0];
-    p.color_ext[ColorsExt::StatusBase as usize] = p.color[Colors::TextDark as usize][2];
-    p.color_ext[ColorsExt::ContainerBase as usize] = p.color[Colors::TextDark as usize][1];
-    p.color_ext[ColorsExt::ContainerBorderFg as usize] = p.color[Colors::Gray as usize][2];
-    p.color_ext[ColorsExt::ContainerArrowFg as usize] = p.color[Colors::Gray as usize][2];
-    p.color_ext[ColorsExt::PopupBase as usize] = p.color[Colors::TextLight as usize][0];
+    p.color_ext[ColorsExt::StatusBase as usize] = p.color[Colors::Black as usize][1];
+    p.color_ext[ColorsExt::ContainerBase as usize] = p.color[Colors::Black as usize][2];
+    p.color_ext[ColorsExt::ContainerBorderFg as usize] = p.color[Colors::Gray as usize][1];
+    p.color_ext[ColorsExt::ContainerArrowFg as usize] = p.color[Colors::Gray as usize][1];
+    p.color_ext[ColorsExt::PopupBase as usize] = p.color[Colors::White as usize][0];
     p.color_ext[ColorsExt::PopupBorderFg as usize] = p.color[Colors::Gray as usize][3];
     p.color_ext[ColorsExt::PopupArrowFg as usize] = p.color[Colors::Gray as usize][3];
-    p.color_ext[ColorsExt::DialogBase as usize] = p.color[Colors::Gray as usize][1];
-    p.color_ext[ColorsExt::DialogBorderFg as usize] = p.color[Colors::Gray as usize][3];
-    p.color_ext[ColorsExt::DialogArrowFg as usize] = p.color[Colors::Gray as usize][3];
+    p.color_ext[ColorsExt::DialogBase as usize] = p.color[Colors::Gray as usize][2];
+    p.color_ext[ColorsExt::DialogBorderFg as usize] = p.color[Colors::Black as usize][3];
+    p.color_ext[ColorsExt::DialogArrowFg as usize] = p.color[Colors::Black as usize][3];
 
     p
 };
