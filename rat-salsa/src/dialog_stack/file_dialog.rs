628517adb2a71f680d10c7264ca41de1ebdf742f
thscharler <thscharler@gmail.com>
2025-11-26 21:23:36 +0100

    feature: add file_dialog_event2()

--- rat-salsa/src/dialog_stack/file_dialog.rs
+++ rat-salsa/src/dialog_stack/file_dialog.rs
@@ -1,5 +1,5 @@
 use crate::{Control, SalsaContext};
-use rat_event::{try_flow, Dialog, HandleEvent};
+use rat_event::{Dialog, HandleEvent, try_flow};
 use rat_widget::event::FileOutcome;
 use rat_widget::file_dialog::{FileDialog, FileDialogState, FileDialogStyle};
 use rat_widget::layout::LayoutOuter;
@@ -76,6 +76,43 @@ where
                     FileOutcome::Ok(f) => {
                         Control::Close(map(Ok(f)))
                     }
+                    r => r.into(),
+                });
+            }
+        } else {
+            panic!("unknown state type");
+        }
+        Ok(Control::Continue)
+    }
+}
+
+/// Create an event-fn for FileDialog to be used with DialogStack.
+#[allow(unused_variables)]
+pub fn file_dialog_event2<Event, Error, Context: SalsaContext<Event, Error>>(
+    map: impl Fn(FileOutcome) -> Event,
+) -> impl Fn(&Event, &mut dyn Any, &mut Context) -> Result<Control<Event>, Error>
+where
+    Event: TryAsRef<crossterm::event::Event> + 'static,
+    Error: From<std::io::Error> + 'static,
+{
+    move |event: &Event, state: &mut dyn Any, ctx: &mut Context| -> Result<Control<Event>, Error> {
+        if let Some(state) = state.downcast_mut::<FileDialogState>() {
+            if let Some(event) = event.try_as_ref() {
+                try_flow!(match state.handle(event, Dialog)? {
+                    r @ FileOutcome::Cancel => Control::Close(map(r)),
+                    r @ FileOutcome::Ok(_) => Control::Close(map(r)),
+                    r @ FileOutcome::OkList(_) => Control::Close(map(r)),
+                    r => r.into(),
+                });
+            }
+        } else if let Some(state) = state.downcast_mut::<Rc<RefCell<FileDialogState>>>() {
+            let mut state = state.borrow_mut();
+
+            if let Some(event) = event.try_as_ref() {
+                try_flow!(match state.handle(event, Dialog)? {
+                    r @ FileOutcome::Cancel => Control::Close(map(r)),
+                    r @ FileOutcome::Ok(_) => Control::Close(map(r)),
+                    r @ FileOutcome::OkList(_) => Control::Close(map(r)),
                     r => r.into(),
                 });
             }
