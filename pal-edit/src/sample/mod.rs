2cf205dd88fb907a7d2747458c4c33f62d136ea1
thscharler <thscharler@gmail.com>
2026-02-09 19:48:06 +0100

    feat: moved a few crates to rat-experimental

--- pal-edit/src/sample/mod.rs
+++
@@ -1,8 +0,0 @@
-mod sample_custom;
-mod sample_data_input;
-mod sample_dialog;
-mod sample_list;
-mod sample_readability;
-mod sample_split;
-mod sample_table;
-pub mod show_sample;
--- pal-edit/src/util/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod clipboard;
-pub mod configparser_ext;
-pub mod message;
